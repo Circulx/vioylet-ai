cd3f38668e444f58cca85997f94c3f3fdeee6c75
skush199 <skush@indosakura.com>
2026-06-17 14:41:10 +0530

    added database conflict

---
+++ alembic/versions/0010_merge_0009_heads.py
@@ -0,0 +1,19 @@
+"""Merge pgvector retrieval and conversation memory heads.
+
+Revision ID: 0010_merge_0009_heads
+Revises: 0009_optional_pgvector_retrieval, 0009_conversation_memory
+Create Date: 2026-06-04
+"""
+
+revision = "0010_merge_0009_heads"
+down_revision = ("0009_optional_pgvector_retrieval", "0009_conversation_memory")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

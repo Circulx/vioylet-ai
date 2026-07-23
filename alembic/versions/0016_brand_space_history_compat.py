8fce07e850b9f74d05c6bab91072fe139f15c287
shruthi merine <smeri@indosakura.com>
2026-07-23 18:36:56 +0530

    mmigration issue

---
+++ alembic/versions/0016_brand_space_history_compat.py
@@ -0,0 +1,19 @@
+"""Compatibility revision for prior Brand Space History migration id.
+
+Revision ID: 0016_brand_space_history
+Revises: 0016_merge_review_heads
+Create Date: 2026-07-23
+"""
+
+revision = "0016_brand_space_history"
+down_revision = "0016_merge_review_heads"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

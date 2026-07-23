7f081dd6c3a39f66946d65526463b9591f3907c1
shruthi merine <smeri@indosakura.com>
2026-07-23 12:54:39 +0530

    history added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -29,6 +29,7 @@ from app.models.brand import (
 from app.models.collaboration import (
     AnalyticsSnapshot,
     BrandCapacityAlertState,
+    BrandSpaceHistory,
     InAppNotification,
     JobRecord,
     ReviewComment,
@@ -55,6 +56,7 @@ __all__ = [
     "BrandLogoAsset",
     "BrandLogoMetadata",
     "BrandSpace",
+    "BrandSpaceHistory",
     "BrandSpaceMember",
     "ChatMessage",
     "ColorPaletteEntry",
